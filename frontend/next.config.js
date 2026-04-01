fc577222209fd0914b36825dddecbd5e2025b3b3
Eniola-Akinlade <splinter0035@gmail.com>
2026-04-01 12:28:07 +0100

    fix: add amplify.yml with appRoot, enable standalone output

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -11,13 +11,4 @@ const nextConfig = {
   },
 }
 
-const { withSentryConfig } = require('@sentry/nextjs');
-
-module.exports = withSentryConfig(nextConfig, {
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
-  silent: !process.env.CI,
-  widenClientFileUpload: true,
-  tunnelRoute: '/monitoring',
-  disableLogger: true,
-});
+module.exports = nextConfig
